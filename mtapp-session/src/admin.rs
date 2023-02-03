4ad356161523632c40b0caac42e997bfcf3f2633
Pouya Mobasher Behrouz <pooyamb@gmail.com>
2023-02-03 10:15:11 +0330

    Blacklist jti on session delete

--- mtapp-session/src/admin.rs
+++ mtapp-session/src/admin.rs
@@ -2,6 +2,7 @@ use axum::extract::Path;
 use axum::response::IntoResponse;
 use axum::Extension;
 use json_response::{JsonListMeta, JsonResponse};
+use mtapp_auth::TokenBlacklist;
 use seaqs::QueryFilter;
 use serde_querystring_axum::QueryString;
 use sqlx::types::Uuid;
@@ -23,8 +24,16 @@ pub async fn list(
 pub async fn batch_delete(
     QueryString(query): QueryString<SessionDeleteFilter>,
     Extension(pool): Extension<PgPool>,
+    blacklist: TokenBlacklist,
 ) -> Result<impl IntoResponse, SessionError> {
     let sessions = Session::delete(&query, &pool).await?;
+
+    for session in sessions.iter() {
+        blacklist
+            .blacklist(session.jti)
+            .await
+            .map_err(|_| SessionError::InternalError)?;
+    }
 
     Ok(JsonResponse::with_content(sessions))
 }
@@ -39,9 +48,15 @@ pub async fn get(
 
 pub async fn delete(
     id: Path<Uuid>,
+    blacklist: TokenBlacklist,
     Extension(pool): Extension<PgPool>,
 ) -> Result<impl IntoResponse, SessionError> {
-    let id = Session::delete_by_id(*id, &pool).await?;
+    let session = Session::delete_by_id(*id, &pool).await?;
 
-    Ok(JsonResponse::with_content(id))
+    blacklist
+        .blacklist(session.jti)
+        .await
+        .map_err(|_| SessionError::InternalError)?;
+
+    Ok(JsonResponse::with_content(session))
 }
